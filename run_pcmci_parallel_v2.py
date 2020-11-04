fcd3e73cef536fd75271cafd4d43402cd4bcf511
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2020-11-04 15:21:34 -0500

    modified where paramters are required

--- run_pcmci_parallel_v2.py
+++ run_pcmci_parallel_v2.py
@@ -13,23 +13,25 @@ import multiprocessing as mp
 
 class PCMCI_Parallel:
 
-    def __init__(self, data: np.ndarray):
+    def __init__(self, data: np.ndarray, tau_max: int, pc_alpha: float):
         self.__nbVar = data.shape[-1]
         self.__data = pp.DataFrame(data)
         self.__cond_ind_test = ParCorr()
+        self.__tau_max = tau_max
+        self.__pc_alpha = pc_alpha
 
     @staticmethod
     def split(container, count):
         return [container[i::count] for i in range(count)]
 
-    def __run_pc_stable_parallel_singleVariable(self, variable, tau_max: int, pc_alpha: float):
+    def __run_pc_stable_parallel_singleVariable(self, variable):
         pcmci_var = PCMCI(dataframe=self.__data, cond_ind_test=self.__cond_ind_test, selected_variables=[variable])
-        parents_of_var = pcmci_var.run_pc_stable(tau_max=tau_max, pc_alpha=pc_alpha)
+        parents_of_var = pcmci_var.run_pc_stable(tau_max=self.__tau_max, pc_alpha=self.__pc_alpha)
         return pcmci_var, parents_of_var
 
-    def __run_PCMCI_parallel(self, variable, tau_max: int, pc_alpha: float):
-        pcmci_var, parents_of_var = self.__run_pc_stable_parallel_singleVariable(variable, tau_max, pc_alpha)
-        results_in_var = pcmci_var.run_mci(tau_max=tau_max)
+    def __run_PCMCI_parallel(self, variable):
+        pcmci_var, parents_of_var = self.__run_pc_stable_parallel_singleVariable(variable)
+        results_in_var = pcmci_var.run_mci(tau_max=self.__tau_max)
         return variable, pcmci_var, parents_of_var, results_in_var
 
     def start(self, nbWorkers: int = None):
@@ -39,6 +41,7 @@ class PCMCI_Parallel:
             nbWorkers = mp.cpu_count()
 
         splittedJobs = self.split(range(self.__nbVar), nbWorkers)
+
         with mp.Pool(nbWorkers) as pool:
             output = pool.map(self.__run_PCMCI_parallel, splittedJobs)
 
