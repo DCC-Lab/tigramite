d83854b56375206acd7a8ae687473dd83fc2ac87
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2020-11-10 10:15:33 -0500

    modified for some performance tests

--- run_pcmci_parallel_v2.py
+++ run_pcmci_parallel_v2.py
@@ -30,10 +30,60 @@ class PCMCI_Parallel:
         pcmci_var = PCMCI(dataframe=self.__data, cond_ind_test=self.__cond_ind_test)
         parents_of_var = pcmci_var.run_pc_stable_singleVar(variable, None, self.__tau_min, self.__tau_max,
                                                            pc_alpha=self.__pc_alpha)
-        return pcmci_var, parents_of_var
+        parents_of_var = {variable: parents_of_var}
+        return variable, pcmci_var, parents_of_var
 
-    def run_PCMCI_parallel(self, variable):
-        pcmci_var, parents_of_var = self.run_pc_stable_parallel_singleVariable(variable)
+    def run_mci_parallel_singleVar(self, variable, pcmci_var, parents_of_var):
+        results_in_var = pcmci_var.run_mci(tau_max=self.__tau_max, parents=parents_of_var)
+        return variable, pcmci_var, parents_of_var, results_in_var
+
+    def start(self, nbWorkers: int = None):
+        if nbWorkers is None:
+            nbWorkers = mp.cpu_count()
+        if nbWorkers > mp.cpu_count():
+            nbWorkers = mp.cpu_count()
+
+        splittedJobs = range(self.__nbVar)
+        chunkSize = len(splittedJobs) // nbWorkers
+        if chunkSize == 0:
+            chunkSize = 1
+
+        with mp.Pool(nbWorkers) as pool:
+            pc_output = pool.map(self.run_pc_stable_parallel_singleVariable, splittedJobs,
+                                 chunksize=chunkSize)
+        with mp.Pool(nbWorkers) as pool:
+            output = pool.starmap(self.run_mci_parallel_singleVar, pc_output, chunksize=chunkSize)
+
+        for result in output:
+            currentParents = result[2]
+            self.all_parents.update(currentParents)
+
+        return output
+
+
+class OlderVariant_PCMCI_Parallel:
+    def __init__(self, data: np.ndarray, tau_min: int, tau_max: int, pc_alpha: float):
+        self.__nbVar = data.shape[-1]
+        self.__data = pp.DataFrame(data)
+        self.__cond_ind_test = ParCorr()
+        self.__tau_max = tau_max
+        self.__tau_min = tau_min
+        self.__pc_alpha = pc_alpha
+        self.all_parents = {}
+
+    @staticmethod
+    def split(container, count):
+        container = tuple(container)
+        return [container[i::count] for i in range(count)]
+
+    def run_pc_stable_parallel_singleVariable(self, variable):
+        pcmci_var = PCMCI(dataframe=self.__data, cond_ind_test=self.__cond_ind_test)
+        parents_of_var = pcmci_var.run_pc_stable_singleVar(variable, None, self.__tau_min, self.__tau_max,
+                                                           pc_alpha=self.__pc_alpha)
+        return variable, pcmci_var, parents_of_var
+
+    def run_mci_parallel_singleVar(self, variable):
+        variabel, pcmci_var, parents_of_var = self.run_pc_stable_parallel_singleVariable(variable)
         results_in_var = pcmci_var.run_mci(tau_max=self.__tau_max)
         return variable, pcmci_var, parents_of_var, results_in_var
 
@@ -44,9 +94,12 @@ class PCMCI_Parallel:
             nbWorkers = mp.cpu_count()
 
         splittedJobs = range(self.__nbVar)
+        chunkSize = len(splittedJobs) // nbWorkers
+        if chunkSize == 0:
+            chunkSize = 1
 
         with mp.Pool(nbWorkers) as pool:
-            output = pool.map(self.run_PCMCI_parallel, splittedJobs)
+            output = pool.map(self.run_mci_parallel_singleVar, splittedJobs)
 
         for result in output:
             currentVar = result[0]
@@ -54,3 +107,34 @@ class PCMCI_Parallel:
             self.all_parents[currentVar] = currentParents
 
         return output
+
+
+if __name__ == '__main__':
+    np.random.seed(42)  # Fix random seed
+    links_coeffs = {0: [((0, -1), 0.7)],
+                    1: [((1, -1), 0.8), ((0, -1), 0.8)],
+                    2: [((2, -1), 0.5), ((1, -2), 0.5)],
+                    }
+
+    T = 500  # time series length
+    data, true_parents_neighbors = pp.var_process(links_coeffs, T=T)
+    T, N = data.shape
+    import os
+    path = os.path.join(os.getcwd(), "tigramite", "data", "timeSeries_ax1.npy")
+    data = np.load(path).T
+    data = data[:440, :100]
+    par = PCMCI_Parallel(data, 1, 5, 0.01)
+    import time
+
+    s = time.time()
+    par.start()
+    print(time.time() - s)
+    print(par.all_parents)
+    norm = PCMCI(pp.DataFrame(data), ParCorr())
+    norm.run_pcmci(tau_min=1, tau_max=5, pc_alpha=0.01)
+    print(norm.all_parents)
+    par_old = OlderVariant_PCMCI_Parallel(data, 1, 5, 0.01)
+    s = time.time()
+    par_old.start()
+    print(time.time() - s)
+    print(par_old.all_parents)
