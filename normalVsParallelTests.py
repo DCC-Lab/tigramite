741d774746c05f7fbcf97c5d4a3fac20380b693c
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2020-11-19 14:03:32 -0500

    new "metric" to test if the parallel method and sequential methods are equivalent

--- normalVsParallelTests.py
+++ normalVsParallelTests.py
@@ -18,6 +18,14 @@ class ParallelVsNormalTest:
         self.__pc_alpha = pc_alpha
         self.__normalAllParents = None
         self.__parallelAllParents = None
+        self.__normalPValMax = None
+        self.__parallelPValMax = None
+        self.__normalValMin = None
+        self.__parallelValMin = None
+        self.__parallelValMatrix = None
+        self.__normalValMatrix = None
+        self.__parallelPValMatrix = None
+        self.__normalPValMatrix = None
 
     def runForBoth(self):
         dataframe = pp.DataFrame(self.__data)
@@ -25,15 +33,19 @@ class ParallelVsNormalTest:
         pcmci_par = run_pcmci_parallel_v2.PCMCI_Parallel(self.__data, ParCorr(), self.__tau_min, self.__tau_max,
                                                          self.__pc_alpha)
         print("==== Running normal PCMCI ====")
-        pcmci_norm.run_pcmci(None, self.__tau_min, self.__tau_max, pc_alpha=self.__pc_alpha)
+        norm_res = pcmci_norm.run_pcmci(None, self.__tau_min, self.__tau_max, pc_alpha=self.__pc_alpha)
         print("==== Running parallel PCMCI ====")
-        pcmci_par.start()
-        self.__normalAllParents = sorted(pcmci_norm.all_parents)
-        self.__parallelAllParents = sorted(pcmci_par.all_parents)
-        self.__normalPValMax = sorted(pcmci_norm.pval_max)
-        self.__parallelPValMax = sorted(pcmci_par.pval_max)
-        self.__normalValMin = sorted(pcmci_norm.val_min)
-        self.__parallelValMin = sorted(pcmci_par.val_min)
+        par_res = pcmci_par.start()
+        self.__normalAllParents = pcmci_norm.all_parents
+        self.__parallelAllParents = pcmci_par.all_parents
+        self.__normalPValMax = pcmci_norm.pval_max
+        self.__parallelPValMax = pcmci_par.pval_max
+        self.__normalValMin = pcmci_norm.val_min
+        self.__parallelValMin = pcmci_par.val_min
+        self.__normalValMatrix = norm_res["val_matrix"]
+        self.__parallelValMatrix = par_res["val_matrix"]
+        self.__normalPValMatrix = norm_res["p_matrix"]
+        self.__parallelPValMatrix = par_res["p_matrix"]
 
     def compareAllParentsAndMCITuples(self, saveDiffToFile: bool = True, diffFilename: str = None):
         if self.__normalAllParents is None or self.__parallelAllParents is None:
@@ -41,6 +53,8 @@ class ParallelVsNormalTest:
         sameParents = self.__normalAllParents == self.__parallelAllParents
         samePValMax = self.__normalPValMax == self.__parallelPValMax
         sameValMin = self.__normalValMin == self.__parallelValMin
+        sameMCIPValMatrix = np.array_equal(self.__parallelPValMatrix, self.__normalPValMatrix)
+        sameMCIValMatrix = np.array_equal(self.__parallelValMatrix, self.__normalValMatrix)
         diff = ""
         if not sameParents:
             diff += f"Sequential parents:\n{self.__normalAllParents}\nParallel parents:\n{self.__parallelAllParents}"
@@ -48,6 +62,10 @@ class ParallelVsNormalTest:
             diff += f"Sequential max pvals:\n{self.__normalPValMax}\nParallel max pvals:\n{self.__normalPValMax}"
         if not sameValMin:
             diff += f"Sequential min vals:\n{self.__normalValMin}\nParallel min vals:\n{self.__parallelValMin}"
+        if not sameMCIPValMatrix:
+            diff += f"Sequential MCI pvals:\n{self.__normalPValMatrix}\nParallel min pvals:\n{self.__parallelPValMatrix}"
+        if not sameMCIValMatrix:
+            diff += f"Sequential MCI vals:\n{self.__normalValMatrix}\nParallel min vals:\n{self.__parallelValMatrix}"
         if diff != "":
             if saveDiffToFile:
                 if diffFilename is None:
